c08f3cca9c6b1e53d1111bcfca4f50b54d25130e
Vishal patil <46629074+vishalpatilweb@users.noreply.github.com>
2024-05-31 17:47:38 +0530

    Testing: Reset Password

---
+++ frontend/__tests__/RequestReset.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import userEvent from '@testing-library/user-event';
+import RequestReset, {
+  REQUEST_RESET_MUTATION,
+} from '../components/RequestReset';
+
+const email = 'editor@sickfits.com';
+const mocks = [
+  {
+    request: {
+      query: REQUEST_RESET_MUTATION,
+      variables: { email },
+    },
+    result: {
+      data: {
+        sendUserPasswordResetLink: null,
+      },
+    },
+  },
+];
+
+describe('<RequestReset/>', () => {
+  it('Renders and matches snapshot', () => {
+    const { container } = render(
+      <MockedProvider>
+        <RequestReset />
+      </MockedProvider>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Calls the mutation when submitted', async () => {
+    const { container, debug } = render(
+      <MockedProvider mocks={mocks}>
+        <RequestReset />
+      </MockedProvider>
+    );
+
+    // type into the email box
+    userEvent.type(screen.getByPlaceholderText(/email/i), email);
+
+    // click submit
+    userEvent.click(screen.getByText(/Request Reset/i));
+    const success = await screen.findByText(/Success/i);
+    expect(success).toBeInTheDocument();
+  });
+});
